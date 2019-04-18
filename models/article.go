ef2b2d70fb2bc5e032f491181819a3d6126a3e42
chg <chg@meitu.com>
2019-04-18 19:01:15 +0800

    增加文章的相关操作

---
+++ models/article.go
@@ -0,0 +1,101 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+/**
+CREATE TABLE `blog_article` (
+  -`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tag_id` int(10) unsigned DEFAULT '0' COMMENT '标签ID',
+  `title` varchar(100) DEFAULT '' COMMENT '文章标题',
+  `desc` varchar(255) DEFAULT '' COMMENT '简述',
+  `content` text,
+  -`created_on` int(11) DEFAULT NULL,
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  -`modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(255) DEFAULT '' COMMENT '修改人',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用1为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章管理'
+*/
+type Article struct {
+	Model
+
+	TagId int `json:"tag_id" gorm:"index"`
+	Tag   Tag `json:"tag"`
+
+	Title      string `json:"title"`
+	Desc       string `json:"desc"`
+	Content    string `json:"content"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State      int    `json:"state"`
+}
+
+func (article *Article) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (article *Article) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
+}
+
+func AddArticle(data map[string]interface{}) bool {
+	db.Create(&Article{
+		TagId:     data["tag_id"].(int),
+		Title:     data["title"].(string),
+		Desc:      data["desc"].(string),
+		Content:   data["content"].(string),
+		CreatedBy: data["created_by"].(string),
+		State:     data["state"].(int),
+	})
+
+	return true
+}
+
+func DeleteArticle(id int) bool {
+	db.Where("id = ?", id).Delete(&Article{})
+
+	return true
+}
+
+func EditArticle(id int, data map[string]interface{}) bool {
+	db.Model(&Article{}).Where("id = ?", id).Updates(data)
+	return true
+}
+
+func GetArticle(id int) (article Article) {
+	db.Where("id = ?", id).First(&article)
+	db.Model(&article).Related(&article.Tag)
+
+	return
+}
+
+func GetArticles(page int, size int, maps map[string]interface{}) (articles []Article) {
+
+	db.Preload("Tag").Where(maps).Offset(page).Limit(page).Find(&articles)
+	return
+}
+
+func GetArticleTotal(maps map[string]interface{}) (count int) {
+	db.Model(&Article{}).Where(maps).Count(&count)
+
+	return
+}
+
+func ExistArticleById(id int) bool {
+	var article Article
+	db.Select("id").Where("id = ?", id).First(&article)
+
+	if article.ID > 0 {
+		return true
+	}
+	return false
+}
--- routers/router.go
+++ routers/router.go
@@ -19,6 +19,7 @@ func InitRouter() *gin.Engine {
 	apiv1 := r.Group("/api/v1")
 
 	{
+		//---标签---
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
 
@@ -30,6 +31,17 @@ func InitRouter() *gin.Engine {
 
 		//删除指定标签
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+		//---文章---
+		apiv1.GET("/articles/:id", v1.GetArticle)
+
+		apiv1.GET("/articles", v1.GetArticles)
+
+		apiv1.POST("/articles", v1.AddArticle)
+
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return r
